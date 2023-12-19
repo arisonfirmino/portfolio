ed9fa2ab804d432c6ed5d0a90642e638a4b261fe
Arison Firmino <arisonfirmino@hotmail.com>
2023-12-19 13:29:30 -0300

    feat: Contact page created

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,4 +1,5 @@
 import { BsChatSquareText } from "react-icons/bs";
+import { MdOutgoingMail } from "react-icons/md";
 
 import ContactForm from "../components/ContactForm";
 
@@ -14,7 +15,24 @@ const Contact = () => {
       </button>
 
       <div className="mt-10 flex w-full flex-col items-center justify-center sm:mt-0 sm:flex-row sm:justify-between sm:px-20">
-        <div className="mb-10 sm:mb-0">Text</div>
+        <div className="mb-10 sm:mb-0">
+          <h1 className="text-4xl font-medium">
+            Send me a <span className="text-primary-color">Email</span>
+          </h1>
+
+          <p className="mt-2 flex items-center">
+            <MdOutgoingMail className="text-primary-color" />
+            &nbsp;
+            <a
+              href="mailto:arisonfirmino@hotmail.com"
+              target="_blank"
+              rel="noreferrer"
+              className="underline decoration-primary-color sm:no-underline sm:hover:underline"
+            >
+              arisonfirmino@hotmail.com
+            </a>
+          </p>
+        </div>
 
         <div className="w-full sm:max-w-lg">
           <ContactForm />
