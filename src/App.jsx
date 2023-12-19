5ae6f678f98d65f6bc0b6278dfde4781a6f3c7a7
Arison Firmino <arisonfirmino@hotmail.com>
2023-12-18 22:17:47 -0300

    chore: tailwind configured

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <main className="flex h-screen w-full items-center justify-center text-text-color">
+      <h1>Hello World!</h1>
+    </main>
+  );
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "bg-color": "#0d1117",
+        "primary-color": "#146ebe",
+        "text-color": "#e1e1e1",
+      },
+    },
+  },
+  plugins: [],
+};
